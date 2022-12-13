23b416af735d2cee3f01dbbf7c7213602b8aa836
hawk198723 <hawk198723@gmail.com>
2022-12-13 00:12:17 -0800

    Dependency Injection

--- spring_04_DI/src/test/java/MyTest.java
+++ spring_04_DI/src/test/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.jason.pojo.Student;
+import com.jason.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,6 +10,16 @@ public class MyTest {
 
         Student student = (Student) context.getBean("student");
         System.out.println(student.toString());
+    }
+
+    @Test
+    public void test(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbean.xml");
+        User user = context.getBean("user2", User.class);
+        User user1 = context.getBean("user2", User.class);
+        System.out.println(user.hashCode());
+        System.out.println(user1.hashCode());// because scope is prototype
+        System.out.println(user);
     }
 }
 
